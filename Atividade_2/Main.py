3bfd28dd888eb4537218c4c6475da5af9c35115c
Rafa7046 <rafael-vilella@hotmail.com>
2022-06-08 03:34:20 -0300

    Comentário

--- Atividade_2/Main.py
+++ Atividade_2/Main.py
@@ -1,6 +1,9 @@
-from pickle import TRUE
 from Graph import Graph
 
+# As variáveis abaixo se referenciam ao grafo que sera analizado e se ele é um grafo orientado ou não,
+# necessário configura-las corretamente para o que código funcione como o esperado.
+# A variável "file" necessita apenas do nome do arquivo dado que ele esteja do diretório "atividade_2", e a sua extensão.
+# A variável "is_oriented" é um booleano que indica se o grafo é orientado ou não.
 
 file = "grafo01.txt"
 is_oriented = False
